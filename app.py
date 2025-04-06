ef3e0e0140c34a728e92a4eb3074cf01051f8ec9
chaitany851P <chaitanythakar851@gmail.com>
2025-04-07 01:23:57 +0530

    error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 def test_route():
     return "Hello, World! This is a test route."
 
-application = app  # For Gunicorn
+application = app
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
